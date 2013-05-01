550a756ebae42fb0b8dbc218e96ef4d7f8cf6aba
elliotjoel <ejb6@cornell.edu>
2013-04-30 23:09:54 -0400

    Cleaned up munging script

--- munge/ncaa_hockey.R
+++ munge/ncaa_hockey.R
@@ -1,38 +1,50 @@
 require(data.table)
 
+# Convert to a data.table
 data <- data.table(ncaa.hockey.scoreboard.1999.2013)
-data$Date <- as.Date(data$Date, "%m/%d/%Y")
-data <- data[order(data$Date),]
-data$Team1[data$Team1 == "Quinnipiac College"] <- "Quinnipiac"
-data$Team2[data$Team2 == "Quinnipiac College"] <- "Quinnipiac"
 
-data <- data[data$GameType != "EX" & data$Loc == "@",]
+# Convert date column to Date type and sort
+data$date <- as.Date(data$date)
+data <- data[order(data$date),]
 
-dateCounts <- data[, list(n = length(Team1)), by = list(Date)]
+# Exclude exhibition games
+data <- data[data$conference != "EX",]
 
-homeData <- data[, list(games = length(Date),
-                        goalsFor = mean(Score2), 
-                        goalsAgainst = mean(Score1),
-                        winPct = mean((Score2 > Score1) + 0.5 * (Score2 == Score1))),
-                 by = list(Team = Team2)]
-roadData <- data[, list(games = length(Date),
-                        goalsFor = mean(Score1),
-                        goalsAgainst = mean(Score2),
-                        winPct = mean((Score1 > Score2) + 0.5 * (Score1 == Score2))),
-                 by = list(Team = Team1)]
+# Get a list of all teams that played conference games. This should be all
+# the D-I schools
+all.teams <- data[data$conference != "NC", list(host, visitor)]
+all.teams <- sort(unique(c(all.teams$host, all.teams$visitor)))
 
-comb.data  <- merge(homeData, roadData, by = "Team", suffixes = c(".home", ".road"))
-comb.data$games <- comb.data$games.home + comb.data$games.road
-comb.data$winPct.total <- (comb.data$winPct.home * comb.data$games.home +
-                             comb.data$winPct.road * comb.data$games.road) / 
-  (comb.data$games.home + comb.data$games.road)
-comb.data <- comb.data[comb.data$games >= 20,]
+# Exclude games involving non-D-I schools
+data <- data[data$host %in% all.teams & data$visitor %in% all.teams,]
 
-adv.data <- data.table(team = comb.data$Team,
-                       winPct.total = comb.data$winPct.total,
-                       winPct = comb.data$winPct.home - comb.data$winPct.road,
-                       goalsFor = comb.data$goalsFor.home - comb.data$goalsFor.road,
-                       goalsAgainst = comb.data$goalsAgainst.home - comb.data$goalsAgainst.road)
-adv.data$goalDifferential <- adv.data$goalsFor - adv.data$goalsAgainst
+# Rename "Quinnipiac College" to "Quinnipiac"
+data$host[data$host == "Quinnipiac College"] <- "Quinnipiac"
+data$visitor[data$visitor == "Quinnipiac College"] <- "Quinnipiac"
 
-correlations <- cor(adv.data[, list(winPct.total, winPct, goalsFor, goalsAgainst, goalDifferential)])
+host.data <- data[, list(games = length(date),
+                         goals.for = mean(host.score), 
+                         goals.against = mean(visitor.score),
+                         win.pct = mean((host.score > visitor.score) + 0.5 * (host.score == visitor.score))),
+                  by = list(season = season, team = host)]
+
+visitor.data <- data[, list(games = length(date),
+                         goals.for = mean(visitor.score),
+                         goals.against = mean(host.score),
+                         win.pct = mean((visitor.score > host.score) + 0.5 * (host.score == visitor.score))),
+                  by = list(season = season, team = visitor)]
+
+agg.data  <- merge(host.data, visitor.data, by = c("season", "team"), suffixes = c(".host", ".visitor"))
+agg.data$games <- agg.data$games.host + agg.data$games.visitor
+agg.data$win.pct <- ((agg.data$win.pct.host * agg.data$games.host +
+                        agg.data$win.pct.visitor * agg.data$games.visitor) / 
+                        agg.data$games)
+
+# agg.data <- agg.data[agg.data$games >= 20,]
+# 
+agg.data$win.pct.adv <- agg.data$win.pct.host - agg.data$win.pct.visitor
+agg.data$goals.for.adv <- agg.data$goals.for.host - agg.data$goals.for.visitor
+agg.data$goals.against.adv <- agg.data$goals.against.host - agg.data$goals.against.visitor
+agg.data$goal.diff.adv <- agg.data$goals.for.adv - agg.data$goals.against.adv
+
+# correlations <- cor(adv.data[, list(winPct.total, winPct, goalsFor, goalsAgainst, goalDifferential)])
